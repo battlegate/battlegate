b507a4a9e2771aa89158a0184740c275867dd519
MacBook <macbook@Macbook.local>
2018-02-25 19:29:36 -0600

    rutas basicas ya hechas y ya sirve el server

--- routes/index.js
+++ routes/index.js
@@ -6,32 +6,32 @@ router.get('/home', (req, res, next) => {
   res.render('index');
 });
 //
-router.get('/createbattle', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/createbattle', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/findbattle', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/findbattle', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/battle', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/battle', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/aboutus', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/aboutus', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/login', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/login', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/signup', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/signup', (req, res, next) => {
+//   res.render('index');
+// });
 
-router.get('/contact', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/contact', (req, res, next) => {
+//   res.render('index');
+// });
 
 module.exports = router;
